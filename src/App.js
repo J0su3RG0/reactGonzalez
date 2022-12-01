4bfe3e421b61d0934abb3455335f88e9042fde57
Josue Gonzalez <DanteOSINT@protonmail.com>
2022-11-30 20:34:45 -0600

    Widget Carrito

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import React, {useState} from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+
+// Importar las paginas de components/pages
+import Home from './components/pages/home';
+import Contacto from './components/pages/contacto';
+import Nosotros from './components/pages/nosotros';
+
+// Importar la NavBar
+import BarraNavegador from './components/navbar/NavBar';
 
 
 function App() {
   return (
     <div className="App">
-      
-      
-
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={ <BarraNavegador /> }>
+            <Route path='home' element={ <Home /> } />
+            <Route path='contacto' element={ <Contacto /> } />
+            <Route path='nosotros' element={ < Nosotros /> } />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -2,6 +2,8 @@ import { Fragment } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { Outlet, Link } from 'react-router-dom';
+import CartWidget from '../cartWidget/index' //CartWidget
 
 
 const BarraNavegador = () => {
@@ -9,16 +11,19 @@ const BarraNavegador = () => {
     <>
       <Navbar bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand as={Link} to="">nombreEmpresa</Navbar.Brand> //Icono de la empresa
+          <Navbar.Brand as={Link} to="">nombreEmpresa</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link as={Link} to="..\pages\home.js">Home</Nav.Link>
             <Nav.Link as={Link} to="..\pages\productos">Productos</Nav.Link>
             <Nav.Link as={Link} to="..\pages\contacto">Contacto</Nav.Link>
             <Nav.Link as={Link} to="..\pages\nosotros">Nosotros</Nav.Link>
-            <Nav.Link as={Link} to="..\pages\carrito">carritoIcono</Nav.Link>
+            <Nav.Link as={Link} to="..\pages\carrito"><CartWidget /></Nav.Link>
           </Nav>
         </Container>
       </Navbar>
+      <section>
+        <Outlet></Outlet>
+      </section>
     </>
   );
 }
